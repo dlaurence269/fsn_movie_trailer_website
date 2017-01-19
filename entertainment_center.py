f3a1dae9f4f430d9199000a6ce6df354ce839225
dlaurence269 <DAL@DAL-MacBook-Pro.fios-router.home>
2017-01-19 08:56:46 -0500

    Cleanse old data in entertainment_center.py
    Eliminate the pieces of code that aren’t relevant to the project, i.e.
    scattered print statements, and add the two movies that were commented
    out, back in to relevance.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,25 +1,20 @@
 import media
 import fresh_tomatoes
 
-#toy_story = media.Movie("Toy Story",
-                        #"A story of a boy and his toys that come to life",
-                        #"https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        #"https://www.youtube.com/watch?v=KYz2wyBy3kc")
-#print (toy_story.storyline)
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-#avatar = media.Movie("Avatar",
-                     #"A marine on an alien planet",
-                     #"https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-                     #"https://www.youtube.com/watch?v=5PSNL1qE6VY")
-#print (avatar.storyline)
-#avatar.show_trailer()
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 casablanca = media.Movie("Casablanca",
                          "A love story and daring escape during WWII",
                          "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/CasablancaPoster-Gold.jpg/440px-CasablancaPoster-Gold.jpg",
                          "https://www.youtube.com/watch?v=a2MnKebNlRo")
-#print (casablanca.storyline)
-#casablanca.show_trailer()
 
 who_framed_roger_rabbit = media.Movie("Who Framed Roger Rabbit",
                                      "An innocent cartoon on the run in the real world",
@@ -32,10 +27,5 @@ trading_places = media.Movie("Trading Places",
                              "https://www.youtube.com/watch?v=ZjDbJQKDXCY")
 
 
-movies = [casablanca, who_framed_roger_rabbit, trading_places]
-#fresh_tomatoes.open_movies_page(movies)
-
-#print (media.Movie.VALID_RATINGS)
-print("Class Documentation: " + media.Movie.__doc__)
-print ("Class Name: " + media.Movie.__name__)
-print ("Class Module: " + media.Movie.__module__)
+movies = [toy_story, avatar, casablanca, who_framed_roger_rabbit, trading_places]
+fresh_tomatoes.open_movies_page(movies)
